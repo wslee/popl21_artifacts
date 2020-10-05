e10b11c70ad0c39c9de6e54729ebfaf76aa36268
Woosuk Lee <wslee@ropas.snu.ac.kr>
2020-10-05 12:04:58 +0900

    update

---
+++ build.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/wslee/duet.git
+git clone https://github.com/wslee/euphony.git
+# build duet 
+cd duet && ./build && cd ..
+# extract benchmarks
+cp -R duet/tests . 
+# build euphony & eusolver
+cd euphony && ./build && cd .. 
+# build CVC4
+tar -xvzf CVC4.tar.gz
