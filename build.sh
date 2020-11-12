81a2494437ec8d3c63a5435ad2160489a90c2f36
Woosuk Lee <wslee@ropas.snu.ac.kr>
2020-11-12 15:13:27 +0900

    freeze duet and euphony

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/wslee/duet.git
-git clone https://github.com/wslee/euphony.git
+#git clone https://github.com/wslee/duet.git
+#git clone https://github.com/wslee/euphony.git
 # build duet 
 cd duet && ./build && eval `opam config env` && make && cd ..
 # extract benchmarks
